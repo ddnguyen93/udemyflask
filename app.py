55eec00cd8bcbb73abe5352d22e1ea6e5db8497f
ddnguyen93 <ddnguyen93@gmail.com>
2021-10-13 12:41:26 -0400

    add run.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
+# from db import db
 
 
 app = Flask(__name__)
@@ -18,9 +18,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "secret"
 api = Api(app)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
+# @app.before_first_request
+# def create_table():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
@@ -34,7 +34,15 @@ api.add_resource(Store, '/store/<string:name>')
 
 api.add_resource(StoreList, '/stores')
 
-db.init_app(app)
+# db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    from db import db
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
