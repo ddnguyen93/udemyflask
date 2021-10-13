2869d0cefbbc9245b3e0f75b4a415aaa2fedd57b
ddnguyen93 <ddnguyen93@gmail.com>
2021-10-13 12:45:47 -0400

    edit app.py

--- app.py
+++ app.py
@@ -36,13 +36,13 @@ api.add_resource(StoreList, '/stores')
 
 # db.init_app(app)
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+# if __name__ == '__main__':
+#     from db import db
+#     db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+#     if app.config['DEBUG']:
+#         @app.before_first_request
+#         def create_tables():
+#             db.create_all()
 
-    app.run(port=5000)
+app.run(port=5000)
