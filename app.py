3aaac4f7d0caa39523ddffe643954b09168f6e31
ddnguyen93 <ddnguyen93@gmail.com>
2021-10-13 17:41:46 -0400

    Edit app.py

--- app.py
+++ app.py
@@ -1,81 +1,3 @@
-# import os
-
-# from flask import Flask
-# from flask_restful import Api
-# from flask_jwt import JWT
-
-# from security import authenticate, identity
-# from resources.user import UserRegister
-# from resources.item import Item, ItemList
-# from resources.store import Store, StoreList
-
-# # # from db import db
-
-
-# # app = Flask(__name__)
-# # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-# # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# # app.secret_key = "secret"
-# # api = Api(app)
-
-# # # @app.before_first_request
-# # # def create_table():
-# # #     db.create_all()
-
-# # jwt = JWT(app, authenticate, identity) # /auth
-
-# # api.add_resource(Item, '/item/<string:name>')
-
-# # api.add_resource(ItemList, '/items')
-
-# # api.add_resource(UserRegister, '/register')
-
-# # api.add_resource(Store, '/store/<string:name>')
-
-# # api.add_resource(StoreList, '/stores')
-
-# # db.init_app(app)
-
-# # if __name__ == '__main__':
-# #     from db import db
-# #     db.init_app(app)
-
-# #     if app.config['DEBUG']:
-# #         @app.before_first_request
-# #         def create_tables():
-# #             db.create_all()
-    
-# #     app.run(port=5000)
-
-
-# app = Flask(__name__)
-
-# app.config['DEBUG'] = True
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.secret_key = 'jose'
-# api = Api(app)
-
-# jwt = JWT(app, authenticate, identity)  # /auth
-
-# api.add_resource(Store, '/store/<string:name>')
-# api.add_resource(Item, '/item/<string:name>')
-# api.add_resource(ItemList, '/items')
-# api.add_resource(StoreList, '/stores')
-
-# api.add_resource(UserRegister, '/register')
-
-# if __name__ == '__main__':
-#     from db import db
-#     db.init_app(app)
-
-#     if app.config['DEBUG']:
-#         @app.before_first_request
-#         def create_tables():
-#             db.create_all()
-#     app.run(port=5000)
-
 import os
 
 from flask import Flask
@@ -87,23 +9,32 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+# from db import db
+
+
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://guxdfjopmdcdch:5f5f31f2f0b9c618fd67287b1b7c6e50837a02a03aaf1e38983ecb44ab48ffe2@ec2-54-166-120-40.compute-1.amazonaws.com:5432/dcamg5u1ajqci5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "secret"
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)  # /auth
+# @app.before_first_request
+# def create_table():
+#     db.create_all()
 
-api.add_resource(Store, '/store/<string:name>')
+jwt = JWT(app, authenticate, identity) # /auth
+
 api.add_resource(Item, '/item/<string:name>')
+
 api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
 
 api.add_resource(UserRegister, '/register')
+
+api.add_resource(Store, '/store/<string:name>')
+
+api.add_resource(StoreList, '/stores')
+
+db.init_app(app)
 
 if __name__ == '__main__':
     from db import db
@@ -113,5 +44,5 @@ if __name__ == '__main__':
         @app.before_first_request
         def create_tables():
             db.create_all()
-
+    
     app.run(port=5000)
